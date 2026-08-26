7aa08dffaf606742b163f62d83220c2b3e0a7557
Abi <generalabi700@gmail.com>
2026-08-26 17:59:18 +0530

    Improved animated profile card styling and functionality

--- script.js
+++ script.js
@@ -2,6 +2,24 @@
 let button= document.getElementById("followBtn");
 let themeBtn= document.getElementById("themeBtn");
 let body=document.querySelector("body");
+let role = document.querySelector(".role");
+let roles = ["PSG iTech","CSE Student","Frontend Learner"];
+let currentRole = 0;
+
+setInterval(function(){
+    role.textContent=roles[currentRole];
+        currentRole+=1;
+        if(currentRole === roles.length){
+            currentRole = 0;
+    }
+},1500);
+let saved = localStorage.getItem("theme");
+if(saved === "dark"){
+    body.classList.add("dark");
+    themeBtn.textContent = "☀️ Light Mode";
+}
+
+
 function updateButton(text,className){
     button.textContent=text;
     button.classList.remove("following");
@@ -26,6 +44,15 @@ button.addEventListener("click",function(){
 });
 themeBtn.addEventListener("click",function(){
     body.classList.toggle("dark");
+    let check= body.classList.contains("dark");
+    if(check===true){
+        localStorage.setItem("theme","dark");
+        themeBtn.textContent = "☀️ Light Mode";
+    }
+    else{
+        localStorage.setItem("theme","light");
+        themeBtn.textContent = "🌙 Dark Mode";
+    }
 });
 
 
